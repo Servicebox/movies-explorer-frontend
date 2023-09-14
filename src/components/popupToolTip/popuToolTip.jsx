494e5963672ce06fc131c11bd80098a25f7d133b
Koznova Tamara <s89062960353@yandex.ru>
2023-09-14 14:51:53 +0300

    ошибка

---
+++ src/components/popupToolTip/popuToolTip.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import successful from "../../images/IconYes.png";
+import unsuccessful from "../../images/IconNo.png";
+
+function PopupTooltip({ onClose, isOpen, isError }) {
+  const popupClass = `popup ppopup_type_info ${isOpen ? "popup_opened" : ""}`;
+
+  return (
+    <div className={popupClass}>
+      <div className="popup__container popup__container_type_info">
+        <button
+          className="popup__close-button popup__close-button_place_info"
+          type="button"
+          onClick={onClose}
+        />
+        <img
+          className="popup__photo-info"
+          src={isError ? unsuccessful : successful}
+          alt="Статус регистрации"
+        />
+        <p className="popup__copyright popup__copyright_place_info ">
+          {" "}
+          {isError
+            ? "Что-то пошло не так! Попробуйте ещё раз."
+            : "Вы успешно зарегистрировались!"}
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default PopupTooltip;
